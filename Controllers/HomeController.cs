29700b76ccf3a6ce98f8d5b52e77e29a5ace1dcf
Govaski <joaopedrogovaski@gmail.com>
2025-06-15 16:24:44 -0300

    Refactoring code and migrating to Blazor

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using Database;
 
 namespace OriginsNewHorizons.Controllers;
 
+[ApiController]
+[Route("api/[controller]")]
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
@@ -14,73 +16,12 @@ public class HomeController : Controller
         _logger = logger;
     }
 
-    public IActionResult Index()
-    {
-        return View("~/Views/Home/Login.cshtml");
-    }
-
-    public IActionResult CharacterList()
-    {
-        return View();
-    }
-
-    public IActionResult CharacterPage()
+    [HttpGet("getCharacter")]
+    public Character GetCharacter()
     {
         using (var context = new DatabaseContext())
         {
-            var ViewPlayer = context.Characters.Where(x => x.Id == 1).Select(x => x).Single();
-            return View(ViewPlayer);
+            return context.Characters.Where(x => x.Id == 1).Select(x => x).Single();
         }
     }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
-
-    //Explosão Cataclisma - In Memorium: Deyvid Santos
-    /*public Character GetCharacterData(DatabaseContext context)
-    {
-        Character PlayerChar = new Character();
-
-        PlayerChar.Strength = context.Characters.Where(x => x.Id == 1).Select(x => x.Strength).Single();
-        PlayerChar.Dexterity = context.Characters.Where(x => x.Id == 1).Select(x => x.Dexterity).Single();
-        PlayerChar.Inteligency = context.Characters.Where(x => x.Id == 1).Select(x => x.Inteligency).Single();
-        PlayerChar.EnergyModifier = context.Characters.Where(x => x.Id == 1).Select(x => x.EnergyModifier).Single();
-        PlayerChar.Charisma = context.Characters.Where(x => x.Id == 1).Select(x => x.Charisma).Single();
-        PlayerChar.Will = context.Characters.Where(x => x.Id == 1).Select(x => x.Will).Single();
-        PlayerChar.Lucky = context.Characters.Where(x => x.Id == 1).Select(x => x.Lucky).Single();
-        PlayerChar.Afinnity = context.Characters.Where(x => x.Id == 1).Select(x => x.Afinnity).Single();
-
-        PlayerChar.Name = context.Characters.Where(x => x.Id == 1).Select(x => x.Name).Single();
-        PlayerChar.Mentor = context.Characters.Where(x => x.Id == 1).Select(x => x.Mentor).Single();
-        PlayerChar.Class = context.Characters.Where(x => x.Id == 1).Select(x => x.Class).Single();
-        PlayerChar.SubClass1 = context.Characters.Where(x => x.Id == 1).Select(x => x.SubClass1).Single();
-        PlayerChar.SubClass2 = context.Characters.Where(x => x.Id == 1).Select(x => x.SubClass2).Single();
-        PlayerChar.Element = context.Characters.Where(x => x.Id == 1).Select(x => x.Element).Single();
-        PlayerChar.Carrier = context.Characters.Where(x => x.Id == 1).Select(x => x.Carrier).Single();
-        PlayerChar.PlayerColor = context.Characters.Where(x => x.Id == 1).Select(x => x.PlayerColor).Single();
-        PlayerChar.TransformedColor = context.Characters.Where(x => x.Id == 1).Select(x => x.TransformedColor).Single();
-        PlayerChar.Nation = context.Characters.Where(x => x.Id == 1).Select(x => x.Nation).Single();
-        PlayerChar.Body = context.Characters.Where(x => x.Id == 1).Select(x => x.Body).Single();
-        PlayerChar.TransformedColor = context.Characters.Where(x => x.Id == 1).Select(x => x.TransformedColor).Single();
-        PlayerChar.Vitality = context.Characters.Where(x => x.Id == 1).Select(x => x.Vitality).Single();
-        PlayerChar.Vigor = context.Characters.Where(x => x.Id == 1).Select(x => x.Vigor).Single();
-        PlayerChar.EnergyStats = context.Characters.Where(x => x.Id == 1).Select(x => x.EnergyStats).Single();
-        PlayerChar.Humanity = context.Characters.Where(x => x.Id == 1).Select(x => x.Humanity).Single();
-        PlayerChar.Sanity = context.Characters.Where(x => x.Id == 1).Select(x => x.Sanity).Single();
-        PlayerChar.Notes = context.Characters.Where(x => x.Id == 1).Select(x => x.Notes).Single();
-        PlayerChar.Apperence = context.Characters.Where(x => x.Id == 1).Select(x => x.Apperence).Single();
-        PlayerChar.Personality = context.Characters.Where(x => x.Id == 1).Select(x => x.Personality).Single();
-        PlayerChar.Lore = context.Characters.Where(x => x.Id == 1).Select(x => x.Lore).Single();
-        PlayerChar.Curiosity = context.Characters.Where(x => x.Id == 1).Select(x => x.Curiosity).Single();
-
-        return PlayerChar;
-    }*/
 }
--- Program.cs
+++ Program.cs
@@ -1,31 +1,38 @@
-using Microsoft.EntityFrameworkCore;
+using Database;
+using OriginsNewHorizons.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddDbContext<DatabaseContext>();
+builder.Services.AddHttpClient();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-app.UseRouting();
 
 app.UseAuthorization();
+app.UseAuthentication();
+
+app.UseRouting();
+app.MapControllers();
+
+app.UseAntiforgery();
+
 
 app.MapStaticAssets();
-app.UseStaticFiles();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
 
 app.Run();
