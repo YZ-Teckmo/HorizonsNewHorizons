8eab43ef5e554277f0f0ca26233c3c80d11e3cc8
YZ-Teckmo <deyvidliviaxmex@gmail.com>
2025-06-18 21:00:17 -0300

    Make some details in other screens and creating all the character create screens

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,4 +33,25 @@ public class HomeController : Controller
             return context.Characters.Where(x => x == x).Select(x => x).ToList();
         }
     }
+
+    [HttpGet("getPlayerSkill/{id:int}/{index:int}")]
+    public PlayerHability GetPlayerSkill(int id, int index)
+    {
+        using (var context = new DatabaseContext())
+        {
+            var player = context.Characters.Where(x => x.Id == id).Single();
+            var hablt = player.Habilities[index];
+            var hability = context.PlayerHabilities.Where(x => x.Id == hablt).Single();
+            return hability;
+        }
+    }
+
+    [HttpGet("getAllPlayerSkills")]
+    public List<PlayerHability> GetAllPlayerSkills(int id)
+    {
+        using (var context = new DatabaseContext())
+        {
+            return context.PlayerHabilities.Where(x => x == x).Select(x => x).ToList();
+        }
+    }
 }
